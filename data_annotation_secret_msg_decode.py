b7775b40055b22918c0df6dc27032f9ae9a870c0
kristijanH1998 <kristijan.hornung1@gmail.com>
2025-01-15 00:06:54 -0800

    code turned into a function

--- data_annotation_secret_msg_decode.py
+++ data_annotation_secret_msg_decode.py
@@ -25,18 +25,18 @@ import pandas as pd
 # else:
 #     print(f"Error: {response.status_code}")
 
-dfs = pd.read_html('https://docs.google.com/document/d/e/2PACX-1vRMx5YQlZNa3ra8dYYxmv-QIQ3YJe8tbI3kqcuC7lQiZm-CSEznKfN_HYNSpoXcZIV3Y_O3YoUB1ecq/pub', encoding='utf-8')
-dfs2 = pd.read_html('https://docs.google.com/document/d/e/2PACX-1vQGUck9HIFCyezsrBSnmENk5ieJuYwpt7YHYEzeNJkIb9OSDdx-ov2nRNReKQyey-cwJOoEKUhLmN9z/pub', encoding='utf-8')
+# dfs = pd.read_html('https://docs.google.com/document/d/e/2PACX-1vRMx5YQlZNa3ra8dYYxmv-QIQ3YJe8tbI3kqcuC7lQiZm-CSEznKfN_HYNSpoXcZIV3Y_O3YoUB1ecq/pub', encoding='utf-8')
+# dfs2 = pd.read_html('https://docs.google.com/document/d/e/2PACX-1vQGUck9HIFCyezsrBSnmENk5ieJuYwpt7YHYEzeNJkIb9OSDdx-ov2nRNReKQyey-cwJOoEKUhLmN9z/pub', encoding='utf-8')
 # for df in dfs:
     # print(df)
-table = dfs2[0]
+# table = dfs2[0]
 # print(table[0].tolist()[1:])
 # print(table[2].tolist()[1:])
 # print(len(table[0].tolist())-1)
 
-char_list = table[1].tolist()[1:]
-x_coords = table[0].tolist()[1:]
-y_coords = table[2].tolist()[1:]
+# char_list = table[1].tolist()[1:]
+# x_coords = table[0].tolist()[1:]
+# y_coords = table[2].tolist()[1:]
 
 # def print_grid(rows, cols, char):
 #     for i in range(rows):
@@ -51,16 +51,33 @@ y_coords = table[2].tolist()[1:]
 # print(max(y_coords))
 # print(table[1].tolist()[1:])
 
-rows = int(max(y_coords))
-cols = int(max(x_coords))
+# rows = int(max(y_coords))
+# cols = int(max(x_coords))
 
-grid = [[' ' for _ in range(cols + 1)] for _ in range(rows + 1)]
+# grid = [[' ' for _ in range(cols + 1)] for _ in range(rows + 1)]
 
 # print(grid)
 # print(grid[0][0])
 
-for index, char in enumerate(char_list):
-    grid[rows - int(y_coords[index])][int(x_coords[index])] = char
+# for index, char in enumerate(char_list):
+#     grid[rows - int(y_coords[index])][int(x_coords[index])] = char
 
-for row in grid:
-    print ("".join(map(str,row)))
+# for row in grid:
+#     print ("".join(map(str,row)))
+
+
+def secret_msg_decode(url):
+    dfs = pd.read_html(url, encoding='utf-8')
+    table = dfs[0]
+    char_list = table[1].tolist()[1:]
+    x_coords = table[0].tolist()[1:]
+    y_coords = table[2].tolist()[1:]
+    rows = int(max(y_coords))
+    cols = int(max(x_coords))
+    grid = [[' ' for _ in range(cols + 1)] for _ in range(rows + 1)]
+    for index, char in enumerate(char_list):
+        grid[rows - int(y_coords[index])][int(x_coords[index])] = char
+    for row in grid:
+        print ("".join(map(str,row)))
+
+secret_msg_decode('https://docs.google.com/document/d/e/2PACX-1vQGUck9HIFCyezsrBSnmENk5ieJuYwpt7YHYEzeNJkIb9OSDdx-ov2nRNReKQyey-cwJOoEKUhLmN9z/pub')
